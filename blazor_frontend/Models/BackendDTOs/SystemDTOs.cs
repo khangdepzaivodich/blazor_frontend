8c58c9ab44b9284b012611abd653c6831e00e518
khangdepzaivodich <phantuankhanghcmue@gmail.com>
2026-04-24 22:04:16 +0700

    feat: forgot password, update docker-compose

--- blazor_frontend/Models/BackendDTOs/SystemDTOs.cs
+++ blazor_frontend/Models/BackendDTOs/SystemDTOs.cs
@@ -30,8 +30,19 @@ namespace blazor_frontend.Models.BackendDTOs
     {
         public string Message { get; set; } = string.Empty;
         public bool Success { get; set; } = false;
+    }
 
-    }   
+    public class ForgotPasswordRequest
+    {
+        public string Email { get; set; } = string.Empty;
+    }
+
+    public class ResetPasswordRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
 
     public class UserDto
     {
